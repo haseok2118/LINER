1ca64921de35b22e2768656dac1cd274643efaf2
haseok2118 <66732566+haseok2118@users.noreply.github.com>
2021-03-11 19:29:52 +0900

    [feature] complete main header

---
+++ src/components/HeaderSection/HeaderElements.js
@@ -0,0 +1,106 @@
+import styled from '@emotion/styled';
+
+export const HeaderContainer = styled.div`
+position: fixed;
+width: 100%;
+height:64px;
+background-color: #FFFFFF;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+export const HeaderWrapper = styled.div`
+  width: 1280px;
+  height: 64px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+  padding-top: 10px;
+  padding-bottom: 10px;
+
+`
+export const Logo = styled.a`
+  width: 36px;
+  height: 36px;
+  background-image: url("liner-logo.svg");
+  background-size: cover;
+  border-radius: 8px;
+  align-self: center;
+`;
+export const Search = styled.div`
+  display: flex;
+  width: 680px;
+  border-radius: 8px;
+  background-color: #EEF1F4;
+  
+`;
+
+export const Input = styled.input`
+  background-color: #EEF1F4;
+  width: 680px;
+  line-hight: 1.2;
+  font-size: 18px;
+  border: none;
+  border-radius: 8px;
+  padding-left: 10px;
+  :focus {
+    outline:0;
+  }
+  ::placeholder {
+    color: #C6C9CB;
+    font-weight: 500;
+  }
+`;
+
+export const SearchButton = styled.span`
+background-image: url('/src/images/search.png');
+`;
+
+export const RightButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const AlertButton = styled.button`
+position: relative;
+  width: 32px;
+  height: 32px;
+  border: none;
+  background-color: #fff;
+  background-image: url('noti.svg');
+  background-size: cover;
+  margin-right: 10px;
+  border-radius: 50%;
+  :hover {
+    cursor: pointer;
+    background-color: #F5F8FA;
+    ::after {
+      position: relative;
+      content:"Notification";
+      padding: 1px;
+      font-size: 12px;
+      background-color: #323235;
+      color: #fff;
+      top: 23px;
+      letter-spacing: 0.8px;
+      border-radius: 2px;
+    }
+  }
+`;
+export const UploadButton = styled.button`
+  width: 100px;
+  height: 40px;
+  color:#787D86;
+  background-color: #fff;
+  border: 1px solid #DDE1E6;
+  border-radius: 8px;
+  font-size: 15px;
+  font-weight: 500;
+  img {
+    margin-right: 8px;
+  }
+  :hover {
+    cursor: pointer;
+    background-color: #F5F8FA;
+  }
+`;
---
+++ src/components/HeaderSection/index.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+import {
+  HeaderContainer,
+  HeaderWrapper,
+  Logo,
+  RightButtonContainer,
+  AlertButton,
+  UploadButton,
+  Search,
+  Input,
+  SearchButton
+} from './HeaderElements'
+
+const HeaderSection = () => {
+  const [open, setOpen] = useState(false);
+  const handleModalStatus = () => {
+    setOpen(!open)
+  }
+  return (
+    <>
+    <HeaderContainer>
+      <HeaderWrapper>
+        <Logo href="#">
+          {/* <img src="logo.png"/> */}
+        </Logo>
+        <Search>
+          <Input type="text"  placeholder="Search on LINER" />
+          <SearchButton />
+        </Search>
+        <RightButtonContainer>
+          <AlertButton >
+          </AlertButton>
+          <UploadButton onClick={handleModalStatus}>
+              <img src="add-highlights.svg" alt="" />
+            PDF
+          </UploadButton>
+        </RightButtonContainer>
+      </HeaderWrapper>
+    </HeaderContainer>
+    {/* {open && <UploadModal />}
+    {!open && <></>} */}
+    </>
+    
+  )
+}
+
+export default HeaderSection;
